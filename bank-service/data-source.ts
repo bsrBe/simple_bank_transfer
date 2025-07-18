a47405e56f998a98e7b14191ad3e0d429befcc41
Bisrat Abrham <bsr7349@gmail.com>
2025-07-18 14:44:59 +0300

    Update data-source.ts

--- bank-service/data-source.ts
+++ bank-service/data-source.ts
@@ -7,10 +7,10 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type : "mariadb",
     host : process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "142536bsr", 
-    database: process.env.DB_NAME    || "simple_bank_transfer_for_Bank_service",
+    port: Number(process.env.DB_PORT) || ****,
+    username: process.env.DB_USERNAME || "****",
+    password: process.env.DB_PASSWORD || "****", 
+    database: process.env.DB_NAME    || "****",
     synchronize: true,
     logging: false,
     entities: [
