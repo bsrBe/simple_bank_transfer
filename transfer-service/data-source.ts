57543e1711b6563768bb20e35040770dbec51a37
Bisrat Abrham <bsr7349@gmail.com>
2025-07-18 14:44:29 +0300

    Update data-source.ts

--- transfer-service/data-source.ts
+++ transfer-service/data-source.ts
@@ -6,10 +6,10 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type : "mariadb",
     host : process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "142536bsr", 
-    database: process.env.DB_NAME    || "transfer_service",
+    port: Number(process.env.DB_PORT) || ****,
+    username: process.env.DB_USERNAME || "****",
+    password: process.env.DB_PASSWORD || "****", 
+    database: process.env.DB_NAME    || "****",
     synchronize: true,
     logging: false,
     entities: [
